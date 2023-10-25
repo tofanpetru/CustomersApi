238a00fbecb3b06fe50b3596a6c11654c6161d2e
Tofan Petru <tofanpetru12@gmail.com>
2023-10-25 14:39:38 +0300

    add swagger

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { Request, Response, NextFunction } from 'express';
+import { swaggerSpec } from './swagger'; // Import the swaggerSpec from your swagger.ts file
 import bodyParser from 'body-parser';
 import customerRoutes from './src/routes/customerRoutes';
 
@@ -14,7 +17,10 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 
 app.use('/customers', customerRoutes);
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
   console.log(`API is available at http://localhost:${port}/customers`);
+  console.log(`Swagger UI is available at http://localhost:${port}/api-docs`);
 });
---
+++ swagger.ts
@@ -0,0 +1,22 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Customer API',
+            version: '1.0.0',
+            description: 'API for managing customers',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+    },
+    apis: ['src/routes/*.ts'],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+export { swaggerSpec };
