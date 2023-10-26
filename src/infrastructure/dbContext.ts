5c38e7ff09670564122f81dc629f30c401a9396b
Tofan Petru <tofanpetru12@gmail.com>
2023-10-26 14:35:00 +0300

    code improvements

--- src/infrastructure/dbContext.ts
+++ src/infrastructure/dbContext.ts
@@ -7,9 +7,40 @@ export const getCustomers = (): Customer[] => {
   return db;
 }
 
+export const getCustomerById = (customerId: number) => {
+
+  if (isNaN(customerId) || customerId < 1) {
+    throw new Error('Invalid customer id: ' + customerId);
+  }
+
+  const customer = db.find((c) => c.id === customerId);
+
+  if (customer) {
+    return customer;
+  }
+
+  throw new Error('Customer with Id' + customerId + ' does not exist');
+};
+
+export const deleteCustomerById = (customerId: number) => {
+  if (isNaN(customerId) || customerId < 1) {
+    throw new Error('Invalid customer ID: ' + customerId);
+  }
+
+  const existingCustomerIndex = db.findIndex((c) => c.id === customerId);
+
+  if (existingCustomerIndex === -1) {
+    throw new Error('Customer with ID ' + customerId + ' does not exist');
+  }
+
+  const deletedCustomer = db.splice(existingCustomerIndex, 1)[0];
+
+  return deletedCustomer;
+};
+
 export const addCustomer = (customer: Customer): Customer => {
   const existingCustomer = db.find((c) => c.email === customer.email);
-//add valdiation for id, name.
+  //add valdiation for id, name.
   if (existingCustomer) {
     throw new Error('Customer with the same email already exists');
   }
@@ -18,6 +49,22 @@ export const addCustomer = (customer: Customer): Customer => {
   return customer;
 };
 
+export const updateCustomer = (customerId: number, updatedCustomer: Customer): Customer | undefined => {
+  const existingCustomerIndex = db.findIndex((c) => c.id === customerId);
+
+  if (existingCustomerIndex === -1) {
+    throw new Error('Customer with the specified id:' + customerId + ' does not exist');
+  }
+
+  const existingCustomer = db[existingCustomerIndex];
+
+  db[existingCustomerIndex] = {
+    ...existingCustomer,
+    name: updatedCustomer.name,
+  };
+
+  return db[existingCustomerIndex];
+};
 
 export const seedDatabase = (numberOfEntries: number): void => {
   for (let i = 1; i <= numberOfEntries; i++) {
--- src/routes/customerRoutes.ts
+++ src/routes/customerRoutes.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import { Customer } from '../models/Customer';
-import { getCustomers, addCustomer } from '../infrastructure/dbContext';
+import { getCustomers, addCustomer, updateCustomer, getCustomerById } from '../infrastructure/dbContext';
 import { CreateCustomerRequestValidator } from '../validation/CreateCustomerRequestValidator';
 import { PaginationService } from '../services/PaginationService';
 
@@ -44,6 +44,42 @@ router.get('/', (req: Request, res: Response) => {
 
 /**
  * @swagger
+ * /customers/{id}:
+ *   get:
+ *     summary: Get a customer by ID
+ *     description: Retrieve a customer by their ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The ID of the customer to retrieve.
+ *     responses:
+ *       200:
+ *         description: A customer with the specified ID.
+ *       404:
+ *         description: Customer not found.
+ */
+router.get('/:id', (req: Request, res: Response) => {
+    try {
+        const customerId = parseInt(req.params.id);
+
+        if (isNaN(customerId) || customerId < 1) {
+            res.status(400).json({ error: 'Invalid customer ID' });
+            return;
+        }
+
+        const customer = getCustomerById(customerId);
+
+        res.status(200).json(customer);
+    } catch (error: any) {
+        res.status(404).json({ error: error.message });
+    }
+});
+
+/**
+ * @swagger
  * /customers:
  *   post:
  *     summary: Create a new customer
@@ -70,5 +106,23 @@ router.post('/', CreateCustomerRequestValidator, (req: Request, res: Response) =
         res.status(500).json({ error: error.message });
     }
 });//todo sa prinzi o exceptie custom, sa ie adaugat in middleware. sa se foloseasca statuscodes in loc de hartcodari
+
+router.put('/:id', (req: Request, res: Response) => {
+    try {
+        const customerId = parseInt(req.params.id);
+        const updatedCustomer: Customer = req.body;
+
+        const updated = updateCustomer(customerId, updatedCustomer);
+
+        if (updated) {
+            res.status(200).json(updated);
+            return;
+        }
+
+        res.status(404).json({ error: 'Customer not found with ID ' + customerId });
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
 export default router;
