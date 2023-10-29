46a21efba2811d638475955c5f0ab8aebc530d73
Tofan Petru <tofanpetru12@gmail.com>
2023-10-29 11:40:51 +0200

    implementation of repository in CustomerRoutes

--- src/IoC/AddDependencyService.ts
+++ src/IoC/AddDependencyService.ts
@@ -3,8 +3,11 @@ import { ErrorHandlingMiddleware } from '../presentation/routes/middlewares/Erro
 import { LoggingMiddleware } from '../presentation/routes/middlewares/LoggingMiddleware';
 import { swaggerSpec } from '../../swagger';
 import swaggerUi from 'swagger-ui-express';
-import customerRoutes from '../presentation/routes/customerRoutes';
 import bodyParser from 'body-parser';
+import { CustomerRoutes } from '../presentation/routes/customerRoutes';
+import { DbContext } from '../repository/dbContext';
+import { GenericRepository } from '../repository/repository/abstract/genericRepository';
+import { Customer } from '../repository/persistence/Customer';
 
 export function registerDependencies(app: Express): void {
     registerCustomMiddleware(app);
@@ -26,7 +29,12 @@ function registerServices(app: Express): void {
     app.use(bodyParser.json());
 }
 
-function registerRoutes(app: Express): void {
-    app.use('/customers', customerRoutes);
+export function registerRoutes(app: Express): void {
+    const dbContext = new DbContext<Customer>(); 
+    const customerRepository = new GenericRepository<Customer>(dbContext); 
+
+    const customerRoutes = new CustomerRoutes(customerRepository);
+
+    app.use('/customers', customerRoutes.registerRoutes());
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
--- src/presentation/routes/customerRoutes.ts
+++ src/presentation/routes/customerRoutes.ts
@@ -1,71 +1,74 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, Router } from 'express';
 import { Customer } from '../../repository/persistence/Customer';
-import CustomerRepository from '../../repository/repository/implementations/customerRepository';
 import { CreateCustomerRequestValidator } from '../validation/CreateCustomerRequestValidator';
 import { PaginationService } from '../../application/services/PaginationService';
-import { DbContext } from '../../repository/dbContext'; 
+import { ICustomerRepository } from '../../repository/repository/interfaces/ICustomerRepository';
 
-const router = express.Router();
-
-const dbContext = new DbContext<Customer>();
-const customerRepository = new CustomerRepository(dbContext);
-
-router.get('/', async (req: Request, res: Response) => {
-    const { page, perPage } = req.query;
-    const customers = await customerRepository.findAll();
-    
-    const paginationService = new PaginationService<Customer>(customers);
-
-    const paginatedData = paginationService.paginate({
-        page: parseInt(page as string) || 1,
-        perPage: parseInt(perPage as string) || 10,
-        controllerName: 'customers',
-    });
-
-    res.json(paginatedData);
-});
-
-router.get('/:id', async (req: Request, res: Response) => {
-    try {
-        const customerId = req.params.id;
-        const customer = await customerRepository.getCustomerById(customerId);
-
-        if (customer) {
-            res.status(200).json(customer);
-        } else {
-            res.status(404).json({ error: 'Customer not found with ID ' + customerId });
-        }
-    } catch (error: any) {
-        res.status(400).json({ error: error.message });
+export class CustomerRoutes {
+    constructor(private customerRepository: ICustomerRepository) {
     }
-});
 
-router.post('/', CreateCustomerRequestValidator, async (req: Request, res: Response) => {
-    try {
-        const newCustomer: Customer = req.body;
-        const addedCustomer = await customerRepository.createCustomer(newCustomer);
+    public registerRoutes(): Router {
+        const router = express.Router();
 
-        res.status(201).json(addedCustomer);
-    } catch (error: any) {
-        res.status(400).json({ error: error.message });
+        router.get('/', async (req: Request, res: Response) => {
+            const { page, perPage } = req.query;
+            const customers = await this.customerRepository.getAll();
+
+            const paginationService = new PaginationService<Customer>(customers);
+
+            const paginatedData = paginationService.paginate({
+                page: parseInt(page as string) || 1,
+                perPage: parseInt(perPage as string) || 10,
+                controllerName: 'customers',
+            });
+
+            res.json(paginatedData);
+        });
+
+        router.get('/:id', async (req: Request, res: Response) => {
+            try {
+                const customerId = req.params.id;
+                const customer = await this.customerRepository.getCustomerById(customerId);
+
+                if (customer) {
+                    res.status(200).json(customer);
+                } else {
+                    res.status(404).json({ error: 'Customer not found with ID ' + customerId });
+                }
+            } catch (error: any) {
+                res.status(400).json({ error: error.message });
+            }
+        });
+
+        router.post('/', CreateCustomerRequestValidator, async (req: Request, res: Response) => {
+            try {
+                const newCustomer: Customer = req.body;
+                const addedCustomer = await this.customerRepository.createCustomer(newCustomer);
+
+                res.status(201).json(addedCustomer);
+            } catch (error: any) {
+                res.status(400).json({ error: error.message });
+            }
+        });
+
+        router.put('/:id', async (req: Request, res: Response) => {
+            try {
+                const customerId = req.params.id;
+                const updatedCustomer: Customer = req.body;
+
+                const updated = await this.customerRepository.updateCustomer(customerId, updatedCustomer);
+
+                if (updated) {
+                    res.status(200).json(updated);
+                } else {
+                    res.status(404).json({ error: 'Customer not found with ID ' + customerId });
+                }
+            } catch (error: any) {
+                res.status(400).json({ error: error.message });
+            }
+        });
+
+        return router;
     }
-});
-
-router.put('/:id', async (req: Request, res: Response) => {
-    try {
-        const customerId = req.params.id;
-        const updatedCustomer: Customer = req.body;
-
-        const updated = await customerRepository.updateCustomer(customerId, updatedCustomer);
-
-        if (updated) {
-            res.status(200).json(updated);
-        } else {
-            res.status(404).json({ error: 'Customer not found with ID ' + customerId });
-        }
-    } catch (error: any) {
-        res.status(400).json({ error: error.message });
-    }
-});
-
-export default router;
+}
