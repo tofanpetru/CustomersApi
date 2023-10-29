c0ccb91337d046f390f9fb78f7a387f2078946a1
Tofan Petru <tofanpetru12@gmail.com>
2023-10-29 18:43:48 +0200

    add swagger docs

--- src/presentation/routes/customerRoutes.ts
+++ src/presentation/routes/customerRoutes.ts
@@ -12,6 +12,38 @@ export class CustomerRoutes {
         const router = express.Router();
         const createCustomerValidator = new CreateCustomerValidator(this.customerRepository);
 
+        /**
+         * @swagger
+         * /:
+         *   get:
+         *     summary: Get a list of customers
+         *     description: Get a paginated list of customers
+         *     tags:
+         *       - Customers
+         *     parameters:
+         *       - name: page
+         *         in: query
+         *         description: Page number
+         *         type: integer
+         *       - name: perPage
+         *         in: query
+         *         description: Items per page
+         *         type: integer
+         *     responses:
+         *       200:
+         *         description: Successfully retrieved customers
+         *         schema:
+         *           type: object
+         *           properties:
+         *             data:
+         *               type: array
+         *               items:
+         *                 $ref: '#/definitions/Customer'
+         *             pagination:
+         *               $ref: '#/definitions/Pagination'
+         *       400:
+         *         description: Bad request
+         */
         router.get('/', async (req: Request, res: Response) => {
             const { page, perPage } = req.query;
             const customers = await this.customerRepository.getAll();
@@ -27,11 +59,33 @@ export class CustomerRoutes {
             res.json(paginatedData);
         });
 
+        /**
+         * @swagger
+         * /:id:
+         *   get:
+         *     summary: Get customer by ID
+         *     description: Get a customer by its ID
+         *     tags:
+         *       - Customers
+         *     parameters:
+         *       - name: id
+         *         in: path
+         *         description: Customer ID
+         *     responses:
+         *       200:
+         *         description: Successfully found the customer
+         *         schema:
+         *           $ref: '#/definitions/Customer'
+         *       404:
+         *         description: Customer not found with the specified ID
+         *       400:
+         *         description: Bad request
+         */
         router.get('/:id', async (req: Request, res: Response) => {
             try {
                 const customerId = req.params.id;
                 const customer = await this.customerRepository.getCustomerById(customerId);
-        
+
                 if (customer) {
                     res.status(HttpStatus.OK).json(customer);
                 } else {
@@ -41,25 +95,76 @@ export class CustomerRoutes {
                 res.status(HttpStatus.BAD_REQUEST).json({ error: error.message });
             }
         });
-        
+
+        /**
+         * @swagger
+         * /:
+         *   post:
+         *     summary: Create a new customer
+         *     description: Create a new customer
+         *     tags:
+         *       - Customers
+         *     parameters:
+         *       - in: body
+         *         name: customer
+         *         description: The new customer
+         *         required: true
+         *         schema:
+         *           $ref: '#/definitions/Customer'
+         *     responses:
+         *       201:
+         *         description: Successfully created the customer
+         *         schema:
+         *           $ref: '#/definitions/Customer'
+         *       400:
+         *         description: Bad request
+         */
         router.post('/', createCustomerValidator.getValidator(), async (req: Request, res: Response) => {
             try {
                 const newCustomer: Customer = req.body;
                 const addedCustomer = await this.customerRepository.createCustomer(newCustomer);
-        
+
                 res.status(HttpStatus.CREATED).json(addedCustomer);
             } catch (error: any) {
                 res.status(HttpStatus.BAD_REQUEST).json({ error: error.message });
             }
         });
-        
+
+        /**
+         * @swagger
+         * /:id:
+         *   put:
+         *     summary: Update customer by ID
+         *     description: Update a customer by its ID
+         *     tags:
+         *       - Customers
+         *     parameters:
+         *       - name: id
+         *         in: path
+         *         description: Customer ID
+         *       - in: body
+         *         name: customer
+         *         description: The updated customer
+         *         required: true
+         *         schema:
+         *           $ref: '#/definitions/Customer'
+         *     responses:
+         *       200:
+         *         description: Successfully updated the customer
+         *         schema:
+         *           $ref: '#/definitions/Customer'
+         *       404:
+         *         description: Customer not found with the specified ID
+         *       400:
+         *         description: Bad request
+         */
         router.put('/:id', async (req: Request, res: Response) => {
             try {
                 const customerId = req.params.id;
                 const updatedCustomer: Customer = req.body;
-        
+
                 const updated = await this.customerRepository.updateCustomer(customerId, updatedCustomer);
-        
+
                 if (updated) {
                     res.status(HttpStatus.OK).json(updated);
                 } else {
@@ -68,13 +173,35 @@ export class CustomerRoutes {
             } catch (error: any) {
                 res.status(HttpStatus.BAD_REQUEST).json({ error: error.message });
             }
-        });    
-        
+        });
+
+        /**
+         * @swagger
+         * /:id:
+         *   delete:
+         *     summary: Delete customer by ID
+         *     description: Delete a customer by its ID
+         *     tags:
+         *       - Customers
+         *     parameters:
+         *       - name: id
+         *         in: path
+         *         description: Customer ID
+         *     responses:
+         *       200:
+         *         description: Successfully deleted the customer
+         *         schema:
+         *           $ref: '#/definitions/Customer'
+         *       404:
+         *         description: Customer not found with the specified ID
+         *       400:
+         *         description: Bad request
+         */
         router.delete('/:id', async (req: Request, res: Response) => {
             try {
                 const customerId = req.params.id;
                 const deletedCustomer = await this.customerRepository.deleteCustomerById(customerId);
-        
+
                 if (deletedCustomer) {
                     res.status(HttpStatus.OK).json(deletedCustomer);
                 } else {
