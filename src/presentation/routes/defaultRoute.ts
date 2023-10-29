35cf6dcdb4a18a3d24ab3631ae59f7b5b0f15a23
Tofan Petru <tofanpetru12@gmail.com>
2023-10-29 21:34:40 +0200

    add swagger docs for default endpoint

--- src/presentation/routes/defaultRoute.ts
+++ src/presentation/routes/defaultRoute.ts
@@ -5,6 +5,22 @@ export class DefaultRoute {
     public registerRoute(): Router {
         const router = express.Router();
 
+        /**
+         * @swagger
+         * /:
+         *   get:
+         *     summary: Get a list of available endpoints
+         *     description: Returns a list of all available endpoints
+         *     tags:
+         *       - Endpoints
+         *     responses:
+         *       200:
+         *         description: Successfully retrieved the list of endpoints
+         *         content:
+         *           application/json:
+         *             example:
+         *               endpoints: ['/customers', '/api-docs']
+         */
         router.get('/', (req: Request, res: Response) => {
             const availableEndpoints = [
                 'http://localhost:3000/customers',
