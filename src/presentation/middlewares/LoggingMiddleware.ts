bfcd1e1eab73b9bdce37abb605756320b696526a
Tofan Petru <tofanpetru12@gmail.com>
2023-10-29 21:56:25 +0200

    small updates

--- src/presentation/middlewares/LoggingMiddleware.ts
+++ src/presentation/middlewares/LoggingMiddleware.ts
@@ -3,14 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 export function LoggingMiddleware(
   req: Request,
   res: Response,
-  next: NextFunction
-) {
-  res.on('finish', () => {
-    console.log(`\n[${new Date().toISOString()}]`);
-    console.log(`Request Method: ${req.method}`);
-    console.log(`Request URL: ${req.url}`);
-    console.log(`Response Status: ${res.statusCode}`);
-  });
-
+  next: NextFunction) {
+  res.on('finish', () => console.log(`[${new Date().toISOString()}] ${req.method} ${req.url} - Status: ${res.statusCode}`));
   next();
 }
