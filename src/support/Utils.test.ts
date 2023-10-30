6e4630c5e52d0e20c330702a3afcdf6a6b508e30
vtasca <valerian.tasca@endava.com>
2023-10-30 15:59:39 +0200

    init

---
+++ src/support/Utils.test.ts
@@ -0,0 +1,10 @@
+import {describe, test, expect} from '@jest/globals';
+
+// 1 unit test per matcher
+
+describe('Utils Test Suite', () => {
+
+    test('add function should add two numbers', ()=> {
+        //TODO: Tobe implemented
+    })
+})
---
+++ src/support/Utils.ts
@@ -0,0 +1,15 @@
+
+export class Utils {
+
+    // https://www.typescriptlang.org/docs/handbook/
+
+    add(a, b) {
+        return a + b;
+    }
+
+    createObjectWithDate() {
+        const obj = {datetime: ''}
+        obj.datetime = '123'
+        return obj;
+    }
+}
