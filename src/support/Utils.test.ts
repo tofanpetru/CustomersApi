7d07826e6442e53bceeebf53ce58cf74ed51cd68
dalexei <dumitru.alexei@endava.com>
2023-11-14 14:38:45 +0200

    Add utils for Unit tests

--- src/support/Utils.test.ts
+++ src/support/Utils.test.ts
@@ -1,10 +1,12 @@
 import {describe, test, expect} from '@jest/globals';
+import {Utils} from "./Utils";
 
 // 1 unit test per matcher
 
 describe('Utils Test Suite', () => {
 
-    test('add function should add two numbers', ()=> {
+    test('add function should add two numbers', () => {
         //TODO: Tobe implemented
+        expect(Utils.sum(1, 2)).toBe(3)
     })
 })
--- src/support/Utils.ts
+++ src/support/Utils.ts
@@ -1,15 +1,33 @@
-
 export class Utils {
 
     // https://www.typescriptlang.org/docs/handbook/
 
-    add(a, b) {
+    static undef: number;
+    static text = "text";
+
+    public static sum(a: number, b: number) {
         return a + b;
     }
 
-    createObjectWithDate() {
+    public static devide(a: number, b: number) {
+        return a / b;
+    }
+
+    public static createObjectWithDate() {
         const obj = {datetime: ''}
-        obj.datetime = '123'
+        obj.datetime = Date.now().toString();
         return obj;
+    }
+
+    public static returnBoolean(v: boolean) {
+        return !v
+    }
+
+    public static creteArray(...args: any[]) {
+        return Array.from(args);
+    }
+
+    public static throwException() {
+        throw new Error("This error is thrown")
     }
 }
