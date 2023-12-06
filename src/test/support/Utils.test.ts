039aa36f0ce402a5549bd73d8a6b56cff8cb0952
dalexei <dumitru.alexei@endava.com>
2023-12-06 16:21:08 +0200

    Add Requirements and placeholders for unit tests

--- src/test/support/Utils.test.ts
+++ src/test/support/Utils.test.ts
@@ -1,12 +1,30 @@
 import {describe, test, expect} from '@jest/globals';
-import { utils } from '../../support/Utils'
-
-// 1 unit test per matcher
+import {Utils, utils} from '../../support/Utils'
 
 describe('Utils Test Suite', () => {
 
-    test('add function should add two numbers', () => {
-        //TODO: Tobe implemented
-        expect(utils.sum(1, 2)).toBe(3)
+    test('Exercise 1: Common matchers', () => {
+        //Assert function utils.sum using common matchers
+    })
+
+    test('Exercise 2: String matchers', () => {
+        //Assert function utils.createObjectWithDate using string matchers
+    })
+
+    test('Exercise 3: Truthiness matcher', () => {
+        //Assert function utils.getLocalVar and utils.setLocalVar using truthiness matchers
+        //utils.returnBoolean can be at use as well
+    })
+
+    test('Exercise 4: Numbers matcher', () => {
+        //Assert function utils.divide using numbers matchers
+    })
+
+    test('Exercise 5: Arrays and Iterables matchers', () => {
+        //Assert function utils.creteArray using Arrays and Iterables matchers
+    })
+
+    test('Exercise 5: Exceptions matchers', () => {
+        //Assert function utils.throwException using Exceptions matchers
     })
 })
